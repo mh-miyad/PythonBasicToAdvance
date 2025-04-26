45f045d2bc2299142482e213803d796c71d3ca81
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2025-04-26 20:02:49 +0600

    updated new number part an cover list part also

---
+++ number.py
@@ -0,0 +1,62 @@
+# start here number section 
+
+# x = 1j
+# a=1
+# b = 1.0
+# c = -12
+# d = -12.56
+# print(type(c))
+# print(type(d))
+# print(type(b))
+# print(type(a))
+# print(type(x))
+
+
+# z = -87.7e100
+# print(z)
+
+# x= int(4.56) # float number make it integer and like this
+# print(x)
+
+# import random
+# print(random.randrange(1,10))
+
+# txt = "The best things in life are free!"
+# print("free" in txt)
+# print("expensive" is txt)
+# b = "Hello, World!"
+# print(b[-5:-2])
+# # print(b[2:])
+# x = 'Welcome'
+# print(x[3:])
+
+# name = "Alice"
+# age = 25
+# print(f"My name is {name} and I am {age} years old")
+
+# # ২. format() মেথড
+# print("My name is {} and I am {} years old".format(name, age))
+
+# # ৩. % অপারেটর
+# # print("My name is %s and I am %d years old" % (name, age))
+# mydict = {83:  80}
+# txt = "Hello Sam!"
+# print(txt.translate(mydict))
+a=bool(False)
+b=bool(None)
+c=bool(0)
+cc=bool("")
+dd=bool(())
+pp=bool([])
+ppp=bool({})
+
+# The `print(a,b,c,cc,dd,pp,ppp)` statement is printing the boolean values of variables `a`, `b`, `c`,
+# `cc`, `dd`, `pp`, and `ppp`. These variables are initialized with the boolean values of `False`,
+# `None`, `0`, `""` (empty string), `()` (empty tuple), `[]` (empty list), and `{}` (empty dictionary)
+# respectively.
+# print(a,b,c,cc,dd,pp,ppp)
+thislist = ["apple", "banana", "cherry","orange"]
+# The `print(thislist[-3:-1])` statement is slicing the list `thislist` to extract a sublist
+# containing elements starting from the third element from the end (index -3) up to, but not
+# including, the last element (index -1).
+print(thislist[-3:-1])
